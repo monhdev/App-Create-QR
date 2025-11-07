6cb976308ff27e89e2dda977efb3a09f04ed4218
monhdev <monproyectocero@gmail.com>
2025-11-07 20:02:39 +0100

    Generador-de-QR-añadido-y-funcionando

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonSegment, IonSegmentButton, IonLabel, IonTextarea } from '@ionic/angular/standalone';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { QrCodeComponent } from 'ng-qrcode';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  standalone: true,
+  imports: [QrCodeComponent, CommonModule, FormsModule, IonHeader, IonToolbar, IonTitle, IonContent, IonSegment, IonSegmentButton, IonLabel, IonTextarea],
 })
 export class HomePage {
+
+  segment = 'generate';//si pusiera generate la página comenzaría con generate, con scan lo mismo
+  qrText = 'Texto del QR';
   constructor() {}
 }
