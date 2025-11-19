f463ebb1048618e4694894375becbffa8f6758b2
monhdev <monproyectocero@gmail.com>
2025-11-19 12:47:54 +0100

    genrador-qr-ya-puede-descargar-imagen-en-modo-web

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -5,7 +5,7 @@ import { CommonModule } from '@angular/common';
 import { QrCodeComponent } from 'ng-qrcode';
 import { shareSocialOutline } from 'ionicons/icons';
 import { addIcons } from 'ionicons';
-
+import html2canvas from 'html2canvas';
 
 
 @Component({
@@ -24,5 +24,24 @@ export class HomePage {
   constructor() {
     //hemos importado addIcons para poder usar los iconos de ionicons y luego lo hemos añadido en el constructor (sino no se veía o daba error)
     addIcons({ shareSocialOutline });
+  }
+
+  captureScreen() {
+    const element = document.getElementById('qrImage') as HTMLElement;
+    html2canvas(element).then((canvas) => {
+      const imgData = canvas.toDataURL('image/png');
+      const link = document.createElement('a');
+      link.download = 'qr.png';
+      link.href = imgData;
+      link.click();
+    });
+  }
+
+    //--download image web--//
+  downloadImage(canvas: HTMLCanvasElement){
+    const link = document.createElement('a');
+    link.download = 'qr.png';
+    link.href = canvas.toDataURL();
+    link.click();
   }
 }
