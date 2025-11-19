8154d30fc7b3c33ec3a54695a7b1cfdd25e3ea03
monhdev <monproyectocero@gmail.com>
2025-11-19 12:12:48 +0100

    generador-qr-funcionando-preparado-para-añadir-descargar-imagen

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,19 +1,28 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonSegment, IonSegmentButton, IonLabel, IonTextarea } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonSegment, IonSegmentButton, IonLabel, IonTextarea, IonButton, IonIcon } from '@ionic/angular/standalone';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { QrCodeComponent } from 'ng-qrcode';
+import { shareSocialOutline } from 'ionicons/icons';
+import { addIcons } from 'ionicons';
+
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [QrCodeComponent, CommonModule, FormsModule, IonHeader, IonToolbar, IonTitle, IonContent, IonSegment, IonSegmentButton, IonLabel, IonTextarea],
+  imports: [ IonIcon, QrCodeComponent, CommonModule, FormsModule, IonHeader, IonToolbar, IonTitle, IonContent, IonSegment, IonSegmentButton, IonLabel, IonTextarea, IonButton],
 })
 export class HomePage {
 
+  ;
+
   segment = 'generate';//si pusiera generate la página comenzaría con generate, con scan lo mismo
-  qrText = 'Texto del QR';
-  constructor() {}
+  qrText = '';
+  constructor() {
+    //hemos importado addIcons para poder usar los iconos de ionicons y luego lo hemos añadido en el constructor (sino no se veía o daba error)
+    addIcons({ shareSocialOutline });
+  }
 }
