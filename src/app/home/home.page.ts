cbbb186985a1351a5effe21cbc2b667b89b09aa4
monhdev <monproyectocero@gmail.com>
2025-12-01 11:54:39 +0100

    preparado-para-grabar-tambien-en-plataformas-moviles-falta-pasarlo-a-android-y-iOS

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,9 @@ import { QrCodeComponent } from 'ng-qrcode';
 import { shareSocialOutline } from 'ionicons/icons';
 import { addIcons } from 'ionicons';
 import html2canvas from 'html2canvas';
-
+import { Filesystem, Directory, Encoding } from "@capacitor/filesystem";
+import { Share } from '@capacitor/share';
+import { LoadingController, Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -17,24 +19,25 @@ import html2canvas from 'html2canvas';
 })
 export class HomePage {
 
-  ;
-
-  segment = 'generate';//si pusiera generate la página comenzaría con generate, con scan lo mismo
   qrText = '';
-  constructor() {
+  constructor(
+    private loadingController: LoadingController,
+    private platform: Platform
+  ) {
     //hemos importado addIcons para poder usar los iconos de ionicons y luego lo hemos añadido en el constructor (sino no se veía o daba error)
     addIcons({ shareSocialOutline });
   }
 
   captureScreen() {
+
     const element = document.getElementById('qrImage') as HTMLElement;
+
     html2canvas(element).then((canvas) => {
-      const imgData = canvas.toDataURL('image/png');
-      const link = document.createElement('a');
-      link.download = 'qr.png';
-      link.href = imgData;
-      link.click();
-    });
+
+      if (this.platform.is('capacitor')) this.shareImage(canvas);
+       else this.downloadImage(canvas);
+      ;
+    })
   }
 
     //--download image web--//
@@ -44,4 +47,33 @@ export class HomePage {
     link.href = canvas.toDataURL();
     link.click();
   }
+
+    //--download image mobile--//
+  async shareImage(canvas: HTMLCanvasElement){
+
+    let base64 = canvas.toDataURL();
+    let path = 'qr.png';
+
+    const loading = await this.loadingController.create({ spinner: 'circles' });
+    await loading.present();
+
+    await Filesystem.writeFile({
+      path,
+      data: base64,
+      directory: Directory.Cache,
+      }).then(async (res) => {
+
+        let uri = res.uri;
+
+        await Share.share({ url: uri });
+
+        await Filesystem.deleteFile({
+          path,
+          directory: Directory.Cache,
+        });
+    }).finally(() => loading.dismiss());
+  }
+
+
+
 }
